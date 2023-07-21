ff8d22e607f2ba6144ae00976ab75eba611fbbcc
AryanK1511 <aryankhurana1511@gmail.com>
2023-07-21 16:54:15 -0400

    Added some files

---
+++ 009- Authentication/vehicles-UI/lib/authenticate.js
@@ -0,0 +1,22 @@
+function setToken(token) {
+    localStorage.setItem('access_token', token);
+}
+
+export async function authenticateUser(user, password) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+      method: 'POST',
+      body: JSON.stringify({ userName: user, password: password }),
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+  
+    const data = await res.json();
+  
+    if (res.status === 200) {
+      setToken(data.token);
+      return true;
+    } else {
+      throw new Error(data.message);
+    }
+}
